945ff0a3ffc541b95ef20c0540e8c1cd86e46912
karnankaviya123 <kkarnank123@gmail.com>
2023-05-30 13:29:25 +0200

    Update Repo.ps1

--- Repo.ps1
+++ Repo.ps1
@@ -1,9 +1,10 @@
 #6f36m3gtwxgpmfhjxzyj6b57i2z4ocwsxdsvfq3n5okfp7j565hq
 
-$personalAccessToken = "6f36m3gtwxgpmfhjxzyj6b57i2z4ocwsxdsvfq3n5okfp7j565hq"
+$PAT = "6f36m3gtwxgpmfhjxzyj6b57i2z4ocwsxdsvfq3n5okfp7j565hq"
+$headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
 
-base64AuthInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalAccessToken)"))
-$headers = @{Authorization=("Basic {0}" -f $base64AuthInfo)}
+#base64AuthInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalAccessToken)"))
+#$headers = @{Authorization=("Basic {0}" -f $base64AuthInfo)}
 
 $result = Invoke-RestMethod -Uri "https://dev.azure.com/karnankali1980/_apis/projects?api-version=6.0" -Method Get -Headers $headers
 
@@ -17,5 +18,9 @@ for ($i=0 ; $i -le $result.count ; $i++)
     
         $project =  $projectNames[$i]
         Write-Host $project
-        #git clone https://ccscandy.visualstudio.com/$project/_git/DevOps $project
+        git clone https://karnan.kali@1980.visualstudio.com/$project/_git/DevOps $project
+        cd $project
+        git add .
+        git commit "migrate"
+        git push
 }
