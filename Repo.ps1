7771347d6398eb7447ff8511694354c92d2a382c
karnankaviya123 <kkarnank123@gmail.com>
2023-05-31 19:41:46 +0200

    Update Repo.ps1

--- Repo.ps1
+++ Repo.ps1
@@ -1,26 +1,17 @@
-#6f36m3gtwxgpmfhjxzyj6b57i2z4ocwsxdsvfq3n5okfp7j565hq
-
-$PAT = "6f36m3gtwxgpmfhjxzyj6b57i2z4ocwsxdsvfq3n5okfp7j565hq"
-$headers = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($PAT)")) }
-
-#base64AuthInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($personalAccessToken)"))
-#$headers = @{Authorization=("Basic {0}" -f $base64AuthInfo)}
-
-$result = Invoke-RestMethod -Uri "https://dev.azure.com/karnankali1980/_apis/projects?api-version=6.0" -Method Get -Headers $headers
-
-$projectNames = $result.value.name
-
-Write-Host $result.count
-
-
-for ($i=0 ; $i -le $result.count ; $i++)
-{
-    
-        $project =  $projectNames[$i]
-        Write-Host $project
-        git clone https://karnan.kali@1980.visualstudio.com/$project/_git/DevOps $project
-        cd $project
-        git add .
-        git commit "migrate"
-        git push
-}
+- script: |
+        MY_PAT=ps4rm6zonfja3xrguayipeis3n62njfzvlcuziu76b5xljax34ca
+        B64_PAT=$(printf "%s"":$MY_PAT" | base64)
+        git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://an-de-ohg-sbi@dev.azure.com/an-de-ohg-sbi/AN-Azure-DevOps-ControlRepo/_git/AN-Azure-DevOps-ControlRepo-non-SAP -b main
+        cd AN-Azure-DevOps-ControlRepo-non-SAP
+        ls -latr
+        echo "Something"
+        cd ../
+        git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://an-de-ohg-sbi@dev.azure.com/an-de-ohg-sbi/AN-Azure-TestProject-ControlRepo/_git/AN-Azure-TestProject-ControlRepo
+        cd AN-Azure-TestProject-ControlRepo
+        ls -latr
+        cd ../AN-Azure-DevOps-ControlRepo-non-SAP
+        cp -p .docs README.md ../AN-Azure-TestProject-ControlRepo
+        git add .
+        git commit -m "added base files"
+        cd ../AN-Azure-TestProject-ControlRepo
+        ls -ltra
