66c49e58d40387bbc2f52c7f6e0294a8b7075bec
karnankaviya123 <kkarnank123@gmail.com>
2023-06-01 17:43:50 +0200

    Update Repo.ps1

--- Repo.ps1
+++ Repo.ps1
@@ -1,1 +1,19 @@
 az repos import create --git-source-url https://github.com/fabrikamprime/fabrikam-open-source --repository fabrikam-open-source
+[17:43] Aishwarya S
+
+
+
+
+az devops configure --defaults organization=https://dev.azure.com/fabrikamprime project="Fabrikam Fiber"
+
+
+
+
+
+	
+            Azure DevOps Services | Sign In
+        
+
+
+
+
