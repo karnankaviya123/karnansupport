738f1e3cadae2bac3938cd843560923270c93934
karnankaviya123 <kkarnank123@gmail.com>
2023-06-01 17:45:27 +0200

    Update Repo.ps1

--- Repo.ps1
+++ Repo.ps1
@@ -1,6 +1,32 @@
-az repos import create --git-source-url https://github.com/fabrikamprime/fabrikam-open-source --repository fabrikam-open-source
-[17:43] Aishwarya S
-
+    MY_PAT=fnncrw7lyiknfpqmv5wrr7bbahvmzo3oszukms3bdstvymko6kha
+    B64_PAT=$(printf "%s"":$MY_PAT" | base64)
+    git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://an-de-ohg-sbi@dev.azure.com/an-de-ohg-sbi/AN-Azure-DevOps-ControlRepo/_git/AN-Azure-DevOps-ControlRepo-non-SAP -b main
+    cd AN-Azure-DevOps-ControlRepo-non-SAP
+    ls -latr
+    echo "Something"
+    cd ../
+    git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://an-de-ohg-sbi@dev.azure.com/an-de-ohg-sbi/AN-Azure-TestProject-ControlRepo/_git/AN-Azure-TestProject-ControlRepo
+    cd AN-Azure-TestProject-ControlRepo
+    ls -latr
+    cd ../AN-Azure-DevOps-ControlRepo-non-SAP
+    cp -R .docs README.md ../AN-Azure-TestProject-ControlRepo
+    ls -latr
+    echo "Testing"
+    pwd
+    cd ../AN-Azure-TestProject-ControlRepo
+    git add .
+    git status
+    echo "Something12"
+    pwd
+    git branch
+    git config --global user.email "X001680@aldi-nord.com"
+    git config --global user.name "Karnan Kali"
+    git remote add origin https://an-de-ohg-sbi@dev.azure.com/an-de-ohg-sbi/AN-Azure-TestProject-ControlRepo/_git/AN-Azure-TestProject-ControlRepo
+    git commit -m "added base files" --no-verify
+    #git push
+    git status
+    cd ../AN-Azure-TestProject-ControlRepo
+    ls -ltra
 
 
 
