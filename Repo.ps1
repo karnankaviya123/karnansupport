ea8c45804c7eff466cf2e9b556e40bef045a038d
karnankaviya123 <kkarnank123@gmail.com>
2023-06-01 17:41:33 +0200

    Update Repo.ps1

--- Repo.ps1
+++ Repo.ps1
@@ -1,17 +1,1 @@
-- script: |
-        MY_PAT=ps4rm6zonfja3xrguayipeis3n62njfzvlcuziu76b5xljax34ca
-        B64_PAT=$(printf "%s"":$MY_PAT" | base64)
-        git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://an-de-ohg-sbi@dev.azure.com/an-de-ohg-sbi/AN-Azure-DevOps-ControlRepo/_git/AN-Azure-DevOps-ControlRepo-non-SAP -b main
-        cd AN-Azure-DevOps-ControlRepo-non-SAP
-        ls -latr
-        echo "Something"
-        cd ../
-        git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone https://an-de-ohg-sbi@dev.azure.com/an-de-ohg-sbi/AN-Azure-TestProject-ControlRepo/_git/AN-Azure-TestProject-ControlRepo
-        cd AN-Azure-TestProject-ControlRepo
-        ls -latr
-        cd ../AN-Azure-DevOps-ControlRepo-non-SAP
-        cp -p .docs README.md ../AN-Azure-TestProject-ControlRepo
-        git add .
-        git commit -m "added base files"
-        cd ../AN-Azure-TestProject-ControlRepo
-        ls -ltra
+az repos import create --git-source-url https://github.com/fabrikamprime/fabrikam-open-source --repository fabrikam-open-source
